5567a7deb005149a11c788d5667b8d1edf4773a2
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2021-09-06 10:54:30 -0400

    Add backgroundable container

--- shared/components/Backgroundable.hpp
+++ shared/components/Backgroundable.hpp
@@ -13,6 +13,9 @@ namespace QuestUI {
 
 namespace QuestUI_Components {
 
+    /**
+     * @brief Modifies the background of the child
+     */
     class Backgroundable : public Component, public ComponentRenderer {
     public:
         explicit Backgroundable(std::string_view backgroundType, ComponentWrapper child, bool replaceExistingBackground = true) : replaceExistingBackground(replaceExistingBackground), backgroundType(backgroundType), child(std::move(child)) {}
@@ -26,5 +29,22 @@ namespace QuestUI_Components {
         const bool replaceExistingBackground;
         std::string backgroundType;
         ComponentWrapper child;
+    };
+
+    /**
+     * @brief Creates a new object and modifies the background
+     */
+    class BackgroundableContainer : public BaseContainer {
+    public:
+        explicit BackgroundableContainer(std::string_view backgroundType, std::initializer_list<ComponentWrapper> children) :  backgroundType(backgroundType), BaseContainer(children) {}
+        explicit BackgroundableContainer(std::string_view backgroundType, std::vector<ComponentWrapper> children) :  backgroundType(backgroundType), BaseContainer(children) {}
+
+    protected:
+        Component* render(UnityEngine::Transform *parentTransform) override;
+
+        QuestUI::Backgroundable* background;
+
+    private:
+        std::string backgroundType;
     };
 }
--- src/components/Backgroundable.cpp
+++ src/components/Backgroundable.cpp
@@ -3,6 +3,8 @@
 #include "questui/shared/BeatSaberUI.hpp"
 #include "questui/shared/CustomTypes/Components/Backgroundable.hpp"
 
+#include "UnityEngine/UI/ContentSizeFitter.hpp"
+
 using namespace QuestUI;
 using namespace QuestUI_Components;
 
@@ -17,6 +19,28 @@ Component* QuestUI_Components::Backgroundable::render(UnityEngine::Transform *pa
             background = getTransform()->get_gameObject()->AddComponent<QuestUI::Backgroundable *>();
     }
     background->ApplyBackground(il2cpp_utils::newcsstr(backgroundType));
+
+    return this;
+}
+
+Component *BackgroundableContainer::render(UnityEngine::Transform *parentTransform) {
+    auto gameObject = UnityEngine::GameObject::New_ctor();
+    gameObject->set_name(il2cpp_utils::newcsstr("BSMLBackground"));
+    gameObject->get_transform()->SetParent(parentTransform, false);
+    gameObject->AddComponent<UnityEngine::UI::ContentSizeFitter*>();
+    background = gameObject->AddComponent<QuestUI::Backgroundable*>();
+
+    auto rectTransform = gameObject->GetComponent<UnityEngine::RectTransform*>();
+    rectTransform->set_anchorMin({0,0});
+    rectTransform->set_anchorMax({1,1});
+    rectTransform->set_sizeDelta({0,0});
+
+    background->ApplyBackground(il2cpp_utils::newcsstr(backgroundType));
+
+    transform = gameObject->get_transform();
+    rendered = true;
+
+    update();
 
     return this;
 }
