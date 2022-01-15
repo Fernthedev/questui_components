662f253ff6d9f91182baa0a8e84a8d2d52de09e5
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2022-01-14 21:06:23 -0400

    Moved UnsafeAny to its own header

--- shared/context.hpp
+++ shared/context.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "key.hpp"
+#include "UnsafeAny.hpp"
 
 #include <concepts>
 #include <tuple>
@@ -10,46 +11,6 @@
 #include "UnityEngine/Transform.hpp"
 
 namespace QUC {
-    struct UnsafeAny {
-        UnsafeAny() = default;
-        UnsafeAny(UnsafeAny const&) = delete;
-
-        template<typename T, typename... TArgs>
-        T& make_any(TArgs&&... args) {
-            if (dtor != nullptr) {
-                dtor(data);
-            }
-            free(data);
-            data = new T(std::forward<TArgs>(args)...);
-            dtor = &destroy_data<T>;
-            return get_any<T>();
-        }
-
-        template<typename T>
-        T& get_any() {
-            return *static_cast<T*>(data);
-        }
-
-        bool has_value() {
-            return data != nullptr;
-        }
-
-        ~UnsafeAny() {
-            if (dtor != nullptr)
-                dtor(data);
-            free(data);
-        }
-
-    private:
-
-        template<typename T>
-        static void destroy_data(void* ptr) {
-            delete reinterpret_cast<T*>(ptr);
-        }
-
-        void* data = nullptr;
-        void(*dtor)(void*) = nullptr;
-    };
 
     struct RenderContext;
 
