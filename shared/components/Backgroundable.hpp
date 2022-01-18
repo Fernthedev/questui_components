8e9801860318b30cb0d61663149176bda48b2f7a
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2022-01-17 20:36:24 -0400

    Fix backgroundable woops

--- shared/components/Backgroundable.hpp
+++ shared/components/Backgroundable.hpp
@@ -28,14 +28,14 @@ namespace QUC {
                 auto res = detail::renderSingle(child, ctx);
                 auto backgroundable = data.getData<QuestUI::Backgroundable*>();
 
-                if (replaceExisting || !backgroundable) {
+                if (!backgroundable) {
                     auto go = res->get_gameObject();
                     backgroundable = go->template AddComponent<QuestUI::Backgroundable*>();
                 }
 
                 backgroundable->ApplyBackground(il2cpp_utils::newcsstr(backgroundType));
 
-                return backgroundable->get_transform();
+                return res;
             }
 
 
@@ -56,7 +56,6 @@ namespace QUC {
             BackgroundableContainer(std::string_view type, TArgs... args) : backgroundType(type), Container<TArgs...>(args...) {}
 
             UnityEngine::Transform* render(RenderContext& ctx, RenderContextChildData& data) {
-                UnityEngine::Transform* transform;
                 auto& container = data.getData<UnityEngine::GameObject*>();
 
                 if (!container) {
@@ -64,9 +63,9 @@ namespace QUC {
                     static auto strName = il2cpp_utils::newcsstr<il2cpp_utils::CreationType::Manual>("BSMLBackground");
                     container->set_name(strName);
 
-                    transform = container->get_transform();
+                    UnityEngine::Transform* transform = container->get_transform();
 
-                    transform->SetParent(&ctx.parentTransform, false);
+                    transform->SetParent(CRASH_UNLESS(&ctx.parentTransform), false);
                     container->AddComponent<UnityEngine::UI::ContentSizeFitter *>();
                     auto background = container->AddComponent<QuestUI::Backgroundable *>();
 
@@ -78,13 +77,10 @@ namespace QUC {
                     background->ApplyBackground(il2cpp_utils::newcsstr(backgroundType));
                 }
 
-                if (!transform)
-                    transform = container->get_transform();
-
                 // Then we render our children to ourselves.
-                auto& ctx2 = data.getChildContext([transform] {return transform; });
+                auto& ctx2 = data.getChildContext([container] {return container->get_transform(); });
                 detail::Container<TArgs...>::render(ctx2, data);
-                return transform;
+                return &ctx2.parentTransform;
             }
         };
     }
