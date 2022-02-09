c08355cfe7ad7bf5b0180d7f02c3bb69d72d663c
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2022-02-09 17:01:13 -0400

    Make RainbowText work without the instance to fix freezes

--- shared/components/misc/RainbowText.hpp
+++ shared/components/misc/RainbowText.hpp
@@ -31,17 +31,17 @@ namespace QUC {
         auto& text = ctx.getChildData(Text::key).getData<TMPro::TextMeshProUGUI *>();
 
         text->StartCoroutine(custom_types::Helpers::CoroutineHelper::New(
-                        rainbowCoroutine(ctx, data, text)));
+                        rainbowCoroutine(ctx, data, *this, text)));
 
 
         return ret;
     }
 
     protected:
-        custom_types::Helpers::Coroutine rainbowCoroutine(RenderContext& ctx, RenderContextChildData& data, TMPro::TextMeshProUGUI*& text) {
+        static custom_types::Helpers::Coroutine rainbowCoroutine(RenderContext& ctx, RenderContextChildData& data, RainbowText rainbowText, TMPro::TextMeshProUGUI*& text) {
             co_yield nullptr;
 
-            auto& childData = ctx.getChildData(Text::key);
+            auto& childData = ctx.getChildData(rainbowText.key);
 
 #pragma clang diagnostic push
 #pragma ide diagnostic ignored "EndlessLoop"
@@ -51,18 +51,18 @@ namespace QUC {
                 if (!text || !text->get_enabled()) co_return;
 
                 // if color has no value or if color
-                if (!color.getData().has_value()) color = UnityEngine::Color(1.0f, 0.0f, 0.0f, 1.0f);
+                if (!rainbowText.color.getData().has_value()) rainbowText.color = UnityEngine::Color(1.0f, 0.0f, 0.0f, 1.0f);
 
                 // https://forum.unity.com/threads/solved-rainbow-hue-shift-over-time-c-script.351751/#post-6651745
                 // Assign HSV values to float h, s & v. (Since data.color is stored in RGB)
                 float h, s, v;
 
-                Sombrero::ColorRGBToHSV(**color, h, s, v);
+                Sombrero::ColorRGBToHSV(*rainbowText.color.getData(), h, s, v);
 
                 // Use HSV values to increase H in HSVToRGB. It looks like putting a value greater than 1 will round % 1 it
-                this->color = Sombrero::ColorHSVToRGB(h + UnityEngine::Time::get_deltaTime() * 0.25f * speed * 0.25f, s, v);
+                rainbowText.color = Sombrero::ColorHSVToRGB(h + UnityEngine::Time::get_deltaTime() * 0.25f * rainbowText.speed * 0.25f, s, v);
 
-                Text::render(ctx, childData);
+                rainbowText.Text::render(ctx, childData);
 
                 co_yield nullptr;
             }
